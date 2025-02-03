118a2cfdaf7615171b282389b9f9b45ed69efced
vitalii-minchuk <mcmin4doutsch@gmail.com>
2025-02-03 07:48:34 +0100

    add basicnstyling to the home page

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,19 +1,24 @@
 import Image from "next/image";
 export default function HomePage() {
   return (
-    <div className="flex justify-between items-center h-screen">
-      <Image 
-        src='https://bytegrad.com/course-assets/youtube/expensestracker/preview.png' 
-        alt='hero img'
-        width={700}
-        height={472}
-        className="rounded-lg"
-       />
-       <div>
-        <h1>
-        Tracker Expense 
-        </h1>
-       </div>
+    <div  className="bg-[#5DC0A8] w-full">
+      <div className="flex justify-between items-center gap-4 h-screen max-w-6xl mx-auto p-4">
+        <Image 
+          src='https://bytegrad.com/course-assets/youtube/expensestracker/preview.png' 
+          alt='hero img'
+          width={700}
+          height={472}
+          className="rounded-lg"
+          />
+        <div className="max-w-[600px]">
+          <h1 className="text-4xl font-semibold text-gray-800">
+            Tracker Expense 
+          </h1>
+          <p  className="text-xl text-gray-800">
+            Keep track of your expenses with this simple and easy to use tracker.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
