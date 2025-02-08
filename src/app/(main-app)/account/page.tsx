efecd62e8243b323d03d085ba19e1417ae610af5
vitalii-minchuk <mcmin4doutsch@gmail.com>
2025-02-08 23:39:46 +0100

    fix lottie build errro

--- src/app/(main-app)/account/page.tsx
+++ src/app/(main-app)/account/page.tsx
@@ -1,9 +1,15 @@
-import { PresentSmallLottie } from "~/lottie/present-small";
+"use client"
+
+import dynamic from "next/dynamic";
+
+import * as animationData from '~/assets/lottie/Giftbox.json'
+
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 
 export default function AccountPage() {
   return (
-    <div>
-      <PresentSmallLottie />
+    <div className="bg-zinc-950 h-screen">
+      <Lottie animationData={animationData} className="w-10 h-10" />
     </div>
   );
 }
