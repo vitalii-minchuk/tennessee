d35ced92e68fed0ff9d65716dd47dc0051e34af0
vitalii-minchuk <mcmin4doutsch@gmail.com>
2025-04-18 00:12:26 +0200

    add redirect to post handler

--- src/app/api/fondy/route.ts
+++ src/app/api/fondy/route.ts
@@ -1,20 +1,14 @@
-import { redirect } from "next/navigation";
-import { NextRequest, NextResponse } from "next/server";
+import { NextApiRequest, NextApiResponse } from "next";
 
-export async function POST(req: NextRequest) {
-  try {
-    const body = await req.json();
-    console.log(body);
-    redirect(`/success?data=${body?.payment_id}`);
-    return NextResponse.json({ success: true }, { status: 200 });
-  } catch (error) {
-    return NextResponse.json(
-      {
-        success: false,
-        message: "Invalid request",
-        error: (error as Error).message,
-      },
-      { status: 400 }
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "POST") {
+    const { order_status, order_id } = req.body;
+
+    return res.redirect(
+      307,
+      `/account?order_id=${order_id}&status=${order_status}`
     );
   }
+
+  res.status(405).end();
 }
