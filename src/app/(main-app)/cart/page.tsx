56d91dd9424ea765aabebdf30f508927768545b2
vitalii-minchuk <mcmin4doutsch@gmail.com>
2025-04-18 15:41:57 +0200

    fix server base url

--- src/app/(main-app)/cart/page.tsx
+++ src/app/(main-app)/cart/page.tsx
@@ -1,7 +1,7 @@
 import { Form } from "~/components/cart/form";
 
 export default async function ThankYouPage() {
-  const products = await fetch(process.env.SERVER_BASE_URA + '/products')
+  const products = await fetch(process.env.PUBLIC_NEXT_SERVER_BASE_URL + '/products')
 
     return (
       <div className="bg-zinc-950 h-screen">
