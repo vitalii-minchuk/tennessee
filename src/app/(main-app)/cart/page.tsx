14e0528a950d181ac16e0e17cc612227cb432b34
vitalii-minchuk <mcmin4doutsch@gmail.com>
2025-04-18 15:46:47 +0200

    refactor fetch products func

--- src/app/(main-app)/cart/page.tsx
+++ src/app/(main-app)/cart/page.tsx
@@ -1,8 +1,19 @@
 import { Form } from "~/components/cart/form";
 
 export default async function ThankYouPage() {
-  const products = await fetch(process.env.PUBLIC_NEXT_SERVER_BASE_URL + '/products')
 
+  const fetchProducts = async () => {
+    try {
+        const response = await fetch(process.env.PUBLIC_NEXT_SERVER_BASE_URL + '/products')
+
+        return await response.json() ?? [];
+    } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error(error);
+        return []
+    }
+};
+  const products = await fetchProducts()
     return (
       <div className="bg-zinc-950 h-screen">
         <Form products={products}/>
