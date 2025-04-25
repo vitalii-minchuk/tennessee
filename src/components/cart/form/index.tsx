f86bae01c2b9ae79949c321b32d7850b5cb3f68f
vitalii-minchuk <96005884+vitalii-minchuk@users.noreply.github.com>
2025-04-25 21:42:54 +0200

    Update index.tsx

--- src/components/cart/form/index.tsx
+++ src/components/cart/form/index.tsx
@@ -21,45 +21,11 @@ export const Form = ({ products }: Props) => {
     return await res.json();
   };
 
-  const createPayment = async () => {
-    try {
-      const response = await fetch("/api/payments", {
-        method: "POST",
 
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          customer: {
-            email: "user@example.com",
-          },
-          totalPrice: 1200,
-          orderItems: [
-            {
-              productId: "68022759cb49a7a06637a3db",
-              sizeId: "67f9163fc7c93cbd18402fd1",
-              quantity: 1,
-            },
-          ],
-        }),
-      });
-
-      return await response.json();
-    } catch (error) {
-      // eslint-disable-next-line no-console
-      console.error(error);
-      return [];
-    }
-  };
   const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
     setText(e.target.value);
   };
 
-  const handlePayment = () => {
-    // createPayment().then((res) => {
-      console.log('+++');
-    // });
-  };
 
   const handleClick = () => {
     fetchSettlements(text).then((res) => {
@@ -71,7 +37,6 @@ export const Form = ({ products }: Props) => {
     <div>
       <Input value={text} onChange={handleChange} />
       <Button onClick={handleClick}>send</Button>
-      <Button onClick={handlePayment}>create</Button>
     </div>
   );
 };
